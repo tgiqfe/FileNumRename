1da4f36421776f0e30c56b32694204bc2a4a8518
tgiqfe <tqji31001@gmail.com>
2023-05-20 23:33:25 +0900

    Update history file locaction.

--- FileNumRename/FileNumRename/Lib/RenameHistory.cs
+++ FileNumRename/FileNumRename/Lib/RenameHistory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -25,7 +26,8 @@ namespace FileNumRename.Lib
         {
             try
             {
-                return JsonSerializer.Deserialize<RenameHistory>(File.ReadAllText(HISTORY_FILE));
+                string historyFile = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), HISTORY_FILE);
+                return JsonSerializer.Deserialize<RenameHistory>(File.ReadAllText(historyFile));
             }
             catch { }
             return new RenameHistory();
@@ -33,7 +35,8 @@ namespace FileNumRename.Lib
 
         public void Save()
         {
-            File.WriteAllText(HISTORY_FILE,
+            string historyFile = Path.Combine(Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName), HISTORY_FILE);
+            File.WriteAllText(historyFile,
                 JsonSerializer.Serialize(
                     this,
                     new JsonSerializerOptions()
